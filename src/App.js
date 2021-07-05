17f732d06b17b1fa04c0c0fa2aff161906e529e9
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-07-05 22:35:29 +0530

    all changed file are deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Layout from "./components/Layout/Layout";
 import { Home, Login, Signup, About, FAQ, Section } from "./pages/PageSrc";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 function App() {
+  const [loading , setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(true)
+    setTimeout(()=>{
+      setloading(false)
+    }, 5000)
+  },[])
+
   return (
+    <div className="App">
+      {
+        loading ?
+        <PropagateLoader color={"#D0021B"} loading={loading} size={20} />
+        :
+        
+
+      
+    
     <Switch>
       <Layout>
         <Route path="/section" exact component={Section} />
@@ -18,6 +37,8 @@ function App() {
         <Footer />
       </Layout>
     </Switch>
+    }
+    </div>
   );
 }
 
